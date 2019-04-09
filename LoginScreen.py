f48b911934513888fd865bf468353ced86d2d52f
elliotmoose <elliot_koh_1997@yahoo.com.sg>
2019-04-09 21:05:47 +0800

    confirming of booking slots done

--- LoginScreen.py
+++ LoginScreen.py
@@ -1,5 +1,6 @@
 from kivy.uix.screenmanager import Screen
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.uix.screenmanager import SlideTransition
 
 from TestData import data as TESTDATA
@@ -7,16 +8,12 @@ from TestData import data as TESTDATA
 class LoginScreen(Screen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        taptologin = Button(text="Tap card to login")
-        taptologin.on_press = self.nextScreen
-        self.add_widget(taptologin)
+        label = Label(text="Tap card to login")        
+        self.add_widget(label)
     
     def on_enter(self, *args):
         super().on_enter(*args)
 
         print('Logged Out: Awaiting Login...')
-        self.parent.dbManager.logout()
-
-    def nextScreen(self):
-        self.parent.dbManager.login(TESTDATA)
+        self.parent.dbManager.logout()            
 
