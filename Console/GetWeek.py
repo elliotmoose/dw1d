7df3778c99e74ed8fa8ac859045dec480116a6fe
shankaikai <tiongshankai97@gmail.com>
2019-04-10 14:34:13 +0800

    Made into function. Now working on creating list with dictionaries

--- Console/GetWeek.py
+++ Console/GetWeek.py
@@ -1,45 +1,33 @@
-# -*- coding: utf-8 -*-
-#
-#import datetime
-#from calendar import monthrange
-##offset by app
-#offset = 0
-##print("Today's date:", str(today))
-#
-#def getcurrentweek(offset):
-#    #grab current values
-#
-##    number_of_days_current_month = monthrange(int(year), int(month)[1]
-#    output = []
-#    year = year[2:]
-#    monday = (date - day) + 7*offset
-#    for days in range(monday, monday+5):
-#        if len(str(days)) < 2:
-#            days = '0'+str(days)
-##        if days > number_of_days_current_month:
-#            
-#        current_date = '{:2}/{:2}/{:2}'.format(days, month, year)
-#        output.append(current_date)
-#    return output
-#
-#print(getcurrentweek(offset))
-
 import dateutil.relativedelta as rdelta
 import datetime as DT
 
+#global today and day variables
 today = DT.date.today()  # today's date
 day = DT.datetime.today().weekday() #which day today is
-#print(day)
-#print(today)
+
+#variable offset
 offset = 3
 
-if offset == 0:
-    idx1 = -1
-    idx2 = 1
-else:
-    idx1 = offset
-    idx2 = offset + 1
+
+
+def getWeek(offset):
+    #takes into account case of 1st page den following pages
+    if offset == 0:
+        idx1 = -1
+        idx2 = 1
+    else:
+        idx1 = offset
+        idx2 = offset + 1
+        
+    out1 = [mon(idx1), tue(idx1), wed(idx1), thu(idx1), fri(idx1)]
+    out2 = [mon(idx2), tue(idx2), wed(idx2), thu(idx2), fri(idx2)]
+    output = out1[:day]+out2[day:] 
+    for i in range(len(output)):
+        output[i] = str(output[i])
     
+    return output
+
+#functions to generate each day    
 def mon(a):
     monday = today + rdelta.relativedelta(weekday=rdelta.MO(a))
     return monday
@@ -60,13 +48,8 @@ def fri(a):
     friday = today + rdelta.relativedelta(weekday=rdelta.FR(a))
     return friday
 
-out1 = [mon(idx1), tue(idx1), wed(idx1), thu(idx1), fri(idx1)]
-out2 = [mon(idx2), tue(idx2), wed(idx2), thu(idx2), fri(idx2)]
-output = out1[:day]+out2[day:] 
-for i in range(len(output)):
-    output[i] = str(output[i])
-
-print(output)
+#test getWeek function, just run
+print (getWeek(offset))
 
 
 
